6337d5b1a0129fbc6d5c311f9de1d11426ba1411
David Bonnaud <dbonnaud123@gmail.com>
2021-08-26 12:00:37 -0700

    got swipe gesture and icons and formatting done, needs data and view restructuring to properly display cards on top of each other

--- quoi-app/View/ContentView.swift
+++ quoi-app/View/ContentView.swift
@@ -9,9 +9,17 @@ import SwiftUI
 import CoreData
 
 struct ContentView: View {
-    // PROPERTY
+    // MARK: - PROPERTIES
     
     @State var entry: String = ""
+    
+    @State var meaning: String = ""
+    
+    @State var flipped = false
+    
+    @GestureState private var dragState = DragState.inactive
+    
+    private var dragAreaThreshold: CGFloat = 65.0
     
     private var isButtonDisabled: Bool {
         entry.isEmpty
@@ -28,13 +36,13 @@ struct ContentView: View {
         animation: .default)
     private var items: FetchedResults<Item>
     
-    // BODY
+    //MARK: - BODY
     
     var body: some View {
         
         if (homeView) {
             VStack {
-                // Header
+                //MARK: - Header
                 
                 Text("Quoi")
                     .font(.system(size: 30, design: .monospaced))
@@ -46,28 +54,76 @@ struct ContentView: View {
                     .background(
                         RoundedRectangle(cornerRadius: 15).stroke(Color.purple, lineWidth: 2)
                     )
-                    
+                    .opacity(dragState.isDragging ? 0.0 : 1.0)
+                    .animation(.default)
+                
+                Spacer()
+                
+                //MARK: - Cards
+                ZStack {
+                    ForEach(items) {item in
+                        Image("photo-paris-france-1")
+                            .resizable()
+                            .cornerRadius(24)
+                            .scaledToFit()
+                            .frame(minWidth: 0, maxWidth: .infinity)
+                            .overlay(
+                                VStack(alignment: .center, spacing: 12) {
+                                    Text(self.flipped ? (item.entry ?? "") : (item.meaning ?? ""))
+                                        .foregroundColor(.white)
+                                        .font(.largeTitle)
+                                        .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                                        .shadow(radius: 1)
+                                        .rotation3DEffect(self.flipped ? Angle(degrees: 180): Angle(degrees: 0), axis: (x: CGFloat(0), y: CGFloat(-10), z: CGFloat(0)))
+                                        .animation(.default)
+                                        
+                                }
+                            )
+                            .rotation3DEffect(self.flipped ? Angle(degrees: 180): Angle(degrees: 0), axis: (x: CGFloat(0), y: CGFloat(10), z: CGFloat(0)))
+                            .animation(.default)
+                            .onTapGesture {
+                                self.flipped.toggle()
+                            }
+                            .gesture(LongPressGesture(minimumDuration: 0.01).sequenced(before: DragGesture()).updating(self.$dragState, body: { (value, state, transaction) in switch value {
+                            case .first(true):
+                                state = .pressing
+                            case .second(true, let drag):
+                                state = .dragging(translation: drag?.translation ?? .zero)
+                            default:
+                                break
+                            }}))
+                            .overlay(
+                                ZStack {
+                                    // next
+                                    Image(systemName: "arrow.right")
+                                        .foregroundColor(.purple)
+                                        .font(.system(size: 128))
+                                        .shadow(color: Color(UIColor(red: 0, green: 0, blue: 0, alpha: 0.2)), radius: 12, x: 0, y: 0)
+                                        .opacity(self.dragState.translation.width > self.dragAreaThreshold ? 1.0 : 0.0)
+                                    
+                                    
+                                    // mastered
+                                    Image(systemName: "checkmark")
+                                        .foregroundColor(.purple)
+                                        .font(.system(size: 128))
+                                        .shadow(color: Color(UIColor(red: 0, green: 0, blue: 0, alpha: 0.2)), radius: 12, x: 0, y: 0)
+                                        .opacity(self.dragState.translation.width < -self.dragAreaThreshold ? 1.0 : 0.0)
+                                }
+                            )
+                            .offset(x: self.dragState.translation.width, y: self.dragState.translation.height)
+                            .scaleEffect(self.dragState.isDragging ? 0.85 : 1.0)
+                            .rotationEffect(Angle(degrees: Double(self.dragState.translation.width / 12)))
+                            .animation(.interpolatingSpring(stiffness: 120, damping: 120))
+                            .padding()
+                        //: image
+                    }
+                }
+                
                     
                 
                 Spacer()
                 
-                // Cards
-                
-                Image("photo-paris-france-1")
-                    .resizable()
-                    .cornerRadius(24)
-                    .scaledToFit()
-                    .frame(minWidth: 0, maxWidth: .infinity)
-                    .overlay(
-                        VStack(alignment: .center, spacing: 12) {
-                            Text("Test")
-                                .foregroundColor(.white)
-                        }
-                    )
-                
-                Spacer()
-                
-                // All entries button
+                //MARK: - All entries button
                 
                 Button(action: {
                     homeView = false
@@ -82,12 +138,20 @@ struct ContentView: View {
                             Capsule().stroke(Color.purple, lineWidth: 2)
                         )
                 })
+                .opacity(dragState.isDragging ? 0.0 : 1.0)
+                .animation(.default)
             }
         } else {
             NavigationView {
                 VStack {
                     VStack(spacing: 16) {
                         TextField("New Entry", text: $entry)
+                            .padding()
+                            .background(
+                                Color(UIColor.systemGray6)
+                            )
+                            .cornerRadius(10)
+                        TextField("Meaning", text: $meaning)
                             .padding()
                             .background(
                                 Color(UIColor.systemGray6)
@@ -113,6 +177,10 @@ struct ContentView: View {
                         ForEach(items) { item in
                             VStack(alignment: .leading) {
                                 Text(item.entry ?? "")
+                                    .font(.headline)
+                                    .fontWeight(.bold)
+                                
+                                Text(item.meaning ?? "")
                                     .font(.headline)
                                     .fontWeight(.bold)
                                 
@@ -144,13 +212,50 @@ struct ContentView: View {
             } //: NAVIGATION
         }
     }
-        // FUNCTION
+    
+    //MARK: - DRAGSTATE
+    
+    enum DragState {
+        case inactive
+        case pressing
+        case dragging(translation: CGSize)
+        
+        var translation: CGSize {
+            switch self {
+            case .inactive, .pressing:
+                return .zero
+            case .dragging(let translation):
+                return translation
+            }
+        }
+        
+        var isDragging: Bool {
+            switch self {
+            case .dragging:
+                return true
+            case .pressing, .inactive:
+                return false
+            }
+        }
+        
+        var isPressing: Bool {
+            switch self {
+            case .pressing, .dragging:
+                return true
+            case .inactive:
+                return false
+            }
+        }
+    }
+    
+        //MARK: - FUNCTION
         
     private func addItem() {
         withAnimation {
             let newItem = Item(context: viewContext)
             newItem.timestamp = Date()
             newItem.entry = entry
+            newItem.meaning = meaning
             newItem.mastered = false
             newItem.id = UUID()
             
@@ -162,6 +267,7 @@ struct ContentView: View {
             }
             
             entry = ""
+            meaning = ""
             hideKeyboard()
         }
     }
@@ -184,7 +290,7 @@ struct ContentView: View {
     }
 }
 
-// PREVIEW
+//MARK: - PREVIEW
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
